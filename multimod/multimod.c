41146ee00ffcb9dcbf3a441d7a33e5e7ac979751
tracer-nju <tracer@nju.edu.cn>
2024-10-14 23:42:03 +0900

    Static hostname: hapolaptop        Icon name: computer-container          Chassis: container       Machine ID: 2b0328b222584560886a3564d917d151          Boot ID: 1008a94bf0db4c46b471a4d8a808f810   Virtualization: wsl Operating System: openSUSE Leap 15.5      CPE OS Name: cpe:/o:opensuse:leap:15.5           Kernel: Linux 5.15.123.1-microsoft-standard-WSL2     Architecture: x86-64  23:42:04  up   1:01,  1 user,  load average: 0.03, 0.03, 0.00

--- multimod/multimod.c
+++ multimod/multimod.c
@@ -1,5 +1,17 @@
 #include <stdint.h>
+#include <stdio.h>
+
+int get_sign(uint64_t a, int i)
+{
+  return (a >> i) & 1;
+}
 
 uint64_t multimod(uint64_t a, uint64_t b, uint64_t m) {
-  return (a * b) % m; // buggy
+  uint64_t result = 0;
+  for (int i = 0; i < 64; ++i)
+  {
+    printf("%d\n", get_sign(a, i));
+  }
+  return result;
+  /* return (a * b) % m; // buggy */
 }
