53683c6a80c7dd241cd1933214c4441b29709220
tracer-nju <tracer@nju.edu.cn>
2024-10-15 00:01:32 +0900

    Static hostname: hapolaptop        Icon name: computer-container          Chassis: container       Machine ID: 2b0328b222584560886a3564d917d151          Boot ID: 1008a94bf0db4c46b471a4d8a808f810   Virtualization: wsl Operating System: openSUSE Leap 15.5      CPE OS Name: cpe:/o:opensuse:leap:15.5           Kernel: Linux 5.15.123.1-microsoft-standard-WSL2     Architecture: x86-64  00:01:32  up   1:21,  1 user,  load average: 0.18, 0.10, 0.05

--- multimod/multimod.c
+++ multimod/multimod.c
@@ -1,19 +1,45 @@
 #include <stdint.h>
 #include <stdio.h>
 
+uint64_t add_mod(uint64_t x, uint64_t y, uint64_t m);
+
 int get_sign(uint64_t a, int i)
 {
   return (a >> i) & 1;
 }
 
+uint64_t mod(uint64_t t, uint64_t m)
+{
+  while (t >= m)
+    t -= m;
+  return t;
+}
+
+uint64_t add_mod(uint64_t x, uint64_t y, uint64_t m)
+{
+  uint64_t t = x + y;
+  if (t < x || t < y)
+    return 0;
+  else
+    return mod(t, m);
+}
+
 uint64_t multimod(uint64_t a, uint64_t b, uint64_t m) {
   uint64_t result = 0;
-  printf("--------------------\n");
+  int sign;
+  uint64_t next_power = b;
+  /* printf("--------------------\n"); */
   for (int i = 0; i < 64; ++i)
   {
-    printf("%d\n", get_sign(a, i));
+    sign = get_sign(a, i);
+    if (sign)
+    {
+      result = add_mod(result, next_power, m);
+    }
+    next_power = add_mod(next_power, next_power, m);
+    /* printf("%d\n", get_sign(a, i)); */
   }
-  printf("--------------------\n");
+  /* printf("--------------------\n"); */
   return result;
   /* return (a * b) % m; // buggy */
 }
