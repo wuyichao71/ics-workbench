c6f21611780bf4841d1668ba63a518b07d8f885a
Yichao Wu <wuyichao71@126.com>
2024-10-23 00:56:23 +0900

    modify the .gitignore

--- additional_script/python_mod.py
+++
@@ -1,7 +0,0 @@
-with open('input', 'r') as f:
-    for line in f:
-        tokens = line.split()
-        a = int(tokens[0])
-        b = int(tokens[1])
-        m = int(tokens[2])
-        print(f"{a} * {b} mod {m} = {(a * b) % m}")
