183bfd99854c174156ef11ada606a6a4db25fbec
tracer-nju <tracer@nju.edu.cn>
2024-10-22 18:00:21 +0900

    Static hostname: hapolaptop        Icon name: computer-container          Chassis: container       Machine ID: 2b0328b222584560886a3564d917d151          Boot ID: cfbf6bdfef3f480186de798212a533c0   Virtualization: wsl Operating System: openSUSE Leap 15.5      CPE OS Name: cpe:/o:opensuse:leap:15.5           Kernel: Linux 5.15.123.1-microsoft-standard-WSL2     Architecture: x86-64  18:00:22  up   6:29,  1 user,  load average: 0.13, 0.05, 0.01

--- multimod/main.c
+++ multimod/main.c
@@ -10,7 +10,13 @@ void test(uint64_t a, uint64_t b, uint64_t m) {
 }
 
 int main() {
-  test(123, 456, 789);
-  test(123, 456, -1ULL);
-  test(-2ULL, -2ULL, -1ULL); // should be 1
+  uint64_t a, b, m;
+  FILE *fp = fopen("input", "r");
+  while (fscanf(fp, U64 U64 U64, &a, &b, &m))
+  {
+    test(a, b, m);
+  }
+  /* test(123, 456, 789); */
+  /* test(123, 456, -1ULL); */
+  /* test(-2ULL, -2ULL, -1ULL); // should be 1 */
 }
