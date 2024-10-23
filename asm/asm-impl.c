2e578b5b8c4b3ac6ae5ef54008535c3e25f7cacf
tracer-nju <tracer@nju.edu.cn>
2024-10-23 12:34:36 +0900

    Static hostname: hapolaptop        Icon name: computer-container          Chassis: container       Machine ID: 2b0328b222584560886a3564d917d151          Boot ID: b4bd22a4d7f0496694b3501992189ef0   Virtualization: wsl Operating System: openSUSE Leap 15.5      CPE OS Name: cpe:/o:opensuse:leap:15.5           Kernel: Linux 5.15.123.1-microsoft-standard-WSL2     Architecture: x86-64  12:34:36  up   0:15,  1 user,  load average: 0.43, 0.22, 0.12

--- asm/asm-impl.c
+++ asm/asm-impl.c
@@ -2,16 +2,14 @@
 #include <string.h>
 
 int64_t asm_add(int64_t a, int64_t b) {
-  asm(
-      "addq %1, %0;"
-      /* "addq %[m], %[t];" */
-      /* :"+r"(a) */
-      /* :"r"(b) */
-      :"+m"(a)
-      :"m"(b)
-      );
-  return a;
-  /* return a + b; */
+  /* asm( */
+  /*     "addq %1, %0;" */
+  /*     /1* "addq %[m], %[t];" *1/ */
+  /*     :"+r"(a) */
+  /*     :"r"(b) */
+  /*     ); */
+  /* return a; */
+  return a + b;
 }
 
 int asm_popcnt(uint64_t x) {
