f84b9cf53e44f95ce30f927f109acc44829f9614
tracer-nju <tracer@nju.edu.cn>
2024-10-23 17:56:47 +0900

    Static hostname: hapolaptop        Icon name: computer-container          Chassis: container       Machine ID: 2b0328b222584560886a3564d917d151          Boot ID: b4bd22a4d7f0496694b3501992189ef0   Virtualization: wsl Operating System: openSUSE Leap 15.5      CPE OS Name: cpe:/o:opensuse:leap:15.5           Kernel: Linux 5.15.123.1-microsoft-standard-WSL2     Architecture: x86-64  17:56:47  up   5:37,  1 user,  load average: 0.13, 0.09, 0.02

--- asm/asm-impl.c
+++ asm/asm-impl.c
@@ -45,7 +45,14 @@ int asm_popcnt(uint64_t x) {
 void *asm_memcpy(void *dest, const void *src, size_t n) {
   int i = 0;
   for (; i < n; i++) 
-    ((char *)dest)[i] =  ((char *)src)[i];
+    asm(
+        "movb (%[src],%[i],1), %%al;"
+        "movb %%al, (%[dest],%[i],1);"
+        :
+        :[dest]"r"(dest), [src]"r"(src), [i]"r"(i)
+        :"rax"
+        )
+    /* ((char *)dest)[i] =  ((char *)src)[i]; */
   /* asm( */
   /*     : */
   /*     :[dest]"r"(dest), [src]"r"(src), [i]"r"(i) */
