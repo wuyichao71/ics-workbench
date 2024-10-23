3005ab14ff4086a28f84b59bc47f18a8b2e36588
tracer-nju <tracer@nju.edu.cn>
2024-10-23 15:13:36 +0900

    Static hostname: hapolaptop        Icon name: computer-container          Chassis: container       Machine ID: 2b0328b222584560886a3564d917d151          Boot ID: b4bd22a4d7f0496694b3501992189ef0   Virtualization: wsl Operating System: openSUSE Leap 15.5      CPE OS Name: cpe:/o:opensuse:leap:15.5           Kernel: Linux 5.15.123.1-microsoft-standard-WSL2     Architecture: x86-64  15:13:36  up   2:54,  1 user,  load average: 0.04, 0.01, 0.00

--- asm/main.c
+++ asm/main.c
@@ -7,6 +7,7 @@ int main() {
   int r = asm_setjmp(buf);
   if (r == 0) {
     assert(asm_add(1234, 5678) == 6912);
+    printf("%d\n", asm_popcnt(0x0123456789abcdefULL));
     assert(asm_popcnt(0x0123456789abcdefULL) == 32);
     // TODO: add more tests here.
     asm_longjmp(buf, 123);
