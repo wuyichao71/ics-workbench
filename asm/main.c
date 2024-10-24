e77840a3756b65a8dfe7a3e71144cb9429bb8ea3
tracer-nju <tracer@nju.edu.cn>
2024-10-24 12:23:59 +0900

    Static hostname: hapolaptop        Icon name: computer-container          Chassis: container       Machine ID: 2b0328b222584560886a3564d917d151          Boot ID: b794df0f537044a798c9b930d01c2cc1   Virtualization: wsl Operating System: openSUSE Leap 15.5      CPE OS Name: cpe:/o:opensuse:leap:15.5           Kernel: Linux 5.15.123.1-microsoft-standard-WSL2     Architecture: x86-64  12:23:59  up   0:44,  1 user,  load average: 0.30, 0.16, 0.07

--- asm/main.c
+++ asm/main.c
@@ -9,14 +9,17 @@ int main() {
     assert(asm_add(1234, 5678) == 6912);
     assert(asm_popcnt(0x0123456789abcdefULL) == 32);
     // TODO: add more tests here.
+    printf("Begin check of 'asm_popcnt'\n");
     assert(asm_popcnt(0b0) == 0);
     assert(asm_popcnt(0b01) == 1);
-    printf("Finish check of asm_popcnt'\n");
+    printf("Finish check of 'asm_popcnt'\n");
+    printf("Begin check of 'asm_memcpy'\n");
     char dest[1024];
     char src[1024] = "1234";
     asm_memcpy(dest, src, 4);
     for (int i = 0; i < 4; i++)
       assert(dest[i] == src[i]);
+    printf("Finish check of 'asm_memcpy'\n");
     asm_longjmp(buf, 123);
   } else {
     assert(r == 123);
